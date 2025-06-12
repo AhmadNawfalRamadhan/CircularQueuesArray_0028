fb687c3ac4bb41014b681ea75a4be973a5af8ecc
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-06-12 10:23:54 +0700

    mengecek apakah antrian hanya memiliki satu element

--- queues.cpp
+++ queues.cpp
@@ -45,5 +45,11 @@ public:
             return;
         }
         cout << "\nElement terhapus: " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT ==  REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 }
