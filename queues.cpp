53720f6b2a1d8625dbc6c13831716ade2a064b54
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-06-12 10:22:19 +0700

    mengecek apakah ada slot yang kosng

--- queues.cpp
+++ queues.cpp
@@ -40,5 +40,10 @@ public:
 
     void remove() {
         // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nElement terhapus: " << queue_array[FRONT] << "\n";
     }
 }
